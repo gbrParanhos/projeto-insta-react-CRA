644bb5396c49d03bbdc7a92a8131f34e326e2665
gbrParanhos <gbrparanhos@gmail.com>
2024-07-02 01:04:25 -0300

    Suggestions dinâmico

--- src/Suggestion.js
+++ src/Suggestion.js
@@ -1,66 +1,16 @@
-const Suggestion = () => {
+const Suggestion = ({suggestionItem:{userSuggestion,status}}) => {
   return(
-  <>
     <div className="sugestao">
       <div className="usuario">
-        <img src="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg"/>
+        <img src={userSuggestion.src} alt={userSuggestion.alt}/>
         <div className="texto">
-          <div className="nome">bad.vibes.memes</div>
-          <div className="razao">Segue você</div>
+          <div className="nome">{userSuggestion.alt}</div>
+          <div className="razao">{status}</div>
         </div>
       </div>
 
       <div className="seguir">Seguir</div>
     </div>
-
-    <div className="sugestao">
-      <div className="usuario">
-        <img src="assets/img/chibirdart.svg" alt="chibirdart"/>
-        <div className="texto">
-          <div className="nome">chibirdart</div>
-          <div className="razao">Segue você</div>
-        </div>
-      </div>
-
-      <div className="seguir">Seguir</div>
-    </div>
-
-    <div className="sugestao">
-      <div className="usuario">
-        <img src="assets/img/razoesparaacreditar.svg" alt="razoesparaacreditar" />
-        <div className="texto">
-          <div className="nome">razoesparaacreditar</div>
-          <div className="razao">Novo no Instagram</div>
-        </div>
-      </div>
-
-      <div className="seguir">Seguir</div>
-    </div>
-
-    <div className="sugestao">
-      <div className="usuario">
-        <img src="assets/img/adorable_animals.svg" alt="adorable_animals"/>
-        <div className="texto">
-          <div className="nome">adorable_animals</div>
-          <div className="razao">Segue você</div>
-        </div>
-      </div>
-
-      <div className="seguir">Seguir</div>
-    </div>
-
-    <div className="sugestao">
-      <div className="usuario">
-        <img src="assets/img/smallcutecats.svg" alt="smallcutecats"/>
-        <div className="texto">
-          <div className="nome">smallcutecats</div>
-          <div className="razao">Segue você</div>
-        </div>
-      </div>
-
-      <div className="seguir">Seguir</div>
-    </div>
-  </>
   )
 }
 
--- src/Suggestions.js
+++ src/Suggestions.js
@@ -1,13 +1,36 @@
 import Suggestion from "./Suggestion";
 
 const Suggestions = () => {
+  const suggestionList = [
+    {
+      userSuggestion: { alt: 'bad.vibes.memes', src: 'assets/img/bad.vibes.memes.svg' },
+      status: 'Segue você'
+    },
+    {
+      userSuggestion: { alt: 'chibirdart', src: 'assets/img/chibirdart.svg' },
+      status: 'Segue você'
+    },
+    {
+      userSuggestion: { alt: 'razoesparaacreditar', src: 'assets/img/razoesparaacreditar.svg' },
+      status: 'Novo no Instagram'
+    },
+    {
+      userSuggestion: { alt: 'adorable_animals', src: 'assets/img/adorable_animals.svg' },
+      status: 'Segue você'
+    },
+    {
+      userSuggestion: { alt: 'smallcutecats', src: 'assets/img/smallcutecats.svg' },
+      status: 'Segue você'
+    }
+  ]
+
   return(
     <div className="sugestoes">
       <div className="titulo">
         Sugestões para você
         <div>Ver tudo</div>
       </div>
-      <Suggestion />
+      {suggestionList.map((suggestionItem,indexItem) => <Suggestion suggestionItem = {suggestionItem} key = {indexItem} />)}
     </div>
   )
 }
