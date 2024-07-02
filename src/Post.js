433f9df798e306c57fcf98d291eb2e24df96fb5a
gbrParanhos <gbrparanhos@gmail.com>
2024-07-02 00:35:43 -0300

    Posts dinâmicos

--- src/Post.js
+++ src/Post.js
@@ -1,77 +1,56 @@
-export default function Post() {
+import { useState } from "react"
+
+const Post = ({postItem}) => {
+
+  const [postState,setPostState] = useState(postItem);
+  const {userPost,contentImg,userLike,countLikes,liked = false,saved = false} = postState
+  
+  const addLike = () => {
+    const sumLike = liked? -1: 1
+    setPostState({...postState, 'countLikes': countLikes + sumLike, 'liked': !liked});
+  }
+
+  const savePost = () => {
+    setPostState({...postState, 'saved': !saved});
+  }
+
   return(
-  <>
-    <div class="post">
-      <div class="topo">
-        <div class="usuario">
-          <img src="assets/img/meowed.svg" alt="meowed"/>
-          meowed
-        </div>
-        <div class="acoes">
-          <ion-icon name="ellipsis-horizontal"></ion-icon>
-        </div>
-      </div>
-
-      <div class="conteudo">
-        <img src="assets/img/gato-telefone.svg" alt="gato-telefone"/>
-      </div>
-
-      <div class="fundo">
-        <div class="acoes">
-          <div>
-            <ion-icon name="heart-outline"></ion-icon>
-            <ion-icon name="chatbubble-outline"></ion-icon>
-            <ion-icon name="paper-plane-outline"></ion-icon>
+      <div className="post">
+        <div className="topo">
+          <div className="usuario">
+            <img src={userPost.src} alt={userPost.alt}/>
+            {userPost.alt}
           </div>
-          <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
+          <div className="acoes">
+            <ion-icon name="ellipsis-horizontal"></ion-icon>
           </div>
         </div>
 
-        <div class="curtidas">
-          <img src="assets/img/respondeai.svg" alt="respondeai"/>
-          <div class="texto">
-            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+        <div className="conteudo">
+          <img src={contentImg.src} alt={contentImg.alt} onClick={() => liked? null: addLike()}/>
+        </div>
+
+        <div className="fundo">
+          <div className="acoes">
+            <div>
+              <ion-icon name={liked? "heart": "heart-outline"} style={liked? {color: 'red'}: {}} onClick={() => addLike()}></ion-icon>
+              <ion-icon name="chatbubble-outline"></ion-icon>
+              <ion-icon name="paper-plane-outline"></ion-icon>
+            </div>
+            <div>
+              <ion-icon name={saved?"bookmark":"bookmark-outline"} onClick={() => savePost()}></ion-icon>
+            </div>
+          </div>
+
+          <div className="curtidas">
+            <img src={userLike.src} alt={userLike.alt}/>
+            <div className="texto">
+              Curtido por <strong>{userLike.alt}</strong> e <strong>outras {Intl.NumberFormat('pt-br').format(countLikes)} pessoas</strong>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-    <div class="post">
-      <div class="topo">
-        <div class="usuario">
-          <img src="assets/img/barked.svg" alt="barked"/>
-          barked
-        </div>
-        <div class="acoes">
-          <ion-icon name="ellipsis-horizontal"></ion-icon>
-        </div>
-      </div>
-
-      <div class="conteudo">
-        <img src="assets/img/dog.svg" alt="dog" />
-      </div>
-
-      <div class="fundo">
-        <div class="acoes">
-          <div>
-            <ion-icon name="heart-outline"></ion-icon>
-            <ion-icon name="chatbubble-outline"></ion-icon>
-            <ion-icon name="paper-plane-outline"></ion-icon>
-          </div>
-          <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
-          </div>
-        </div>
-
-        <div class="curtidas">
-          <img src="assets/img/adorable_animals.svg" alt="adorable_animals"/>
-          <div class="texto">
-            Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-          </div>
-        </div>
-      </div>
-    </div>
-  </>
-  )
+    )
 }
+
+export default Post
--- src/Posts.js
+++ src/Posts.js
@@ -1,9 +1,32 @@
 import Post from "./Post";
 
-export default function Posts() {
+const Posts = () => {
+  const postList = [
+    {
+      userPost: { alt: 'meowed', src: 'assets/img/meowed.svg' },
+      contentImg: { alt: 'gato-telefone', src: 'assets/img/gato-telefone.svg' },
+      userLike: { alt: 'respondeai', src: 'assets/img/respondeai.svg' },
+      countLikes: 101523
+    },
+    {
+      userPost: { alt: 'barked', src: 'assets/img/barked.svg' },
+      contentImg: { alt: 'dog', src: 'assets/img/dog.svg' },
+      userLike: { alt: 'adorable_animals', src: 'assets/img/adorable_animals.svg' },
+      countLikes: 99159
+    },
+    {
+      userPost: { alt: 'meowed', src: 'assets/img/meowed.svg' },
+      contentImg: { alt: 'gato-telefone', src: 'assets/img/gato-telefone.svg' },
+      userLike: { alt: 'respondeai', src: 'assets/img/respondeai.svg' },
+      countLikes: 28648
+    }
+  ]
+
   return(
-    <div class="posts">
-      <Post />
+    <div className="posts">
+      {postList.map((postItem,indexItem) => <Post postItem = {postItem} key = {indexItem} />)}
     </div>
   )
 }
+
+export default Posts
