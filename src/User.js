166109c64adf32ac01b0927042a301e95761f153
gbrParanhos <gbrparanhos@gmail.com>
2024-07-02 00:35:33 -0300

    Usuário dinâmico

--- src/User.js
+++ src/User.js
@@ -1,13 +1,36 @@
-export default function User() {
+import React from "react";
+
+const User = () => {
+  const [name,setName] = React.useState('Anônimo');
+  const [urlImg, setUrlImg] = React.useState('assets/img/Anonimo.png')
+
+  const changeName = () => {
+    const questionName = prompt('Qual nome deseja utilizar?');
+
+    if(questionName) {
+      setName(questionName);
+    }
+  }
+
+  const changeUrlImg= () => {
+    const questionImg = prompt('Qual imagem deseja utilizar?');
+
+    if(questionImg) {
+      setUrlImg(questionImg);
+    }
+  }
+
   return(
-    <div class="usuario">
-      <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
-      <div class="texto">
+    <div className="usuario">
+      <img onClick={changeUrlImg} src={urlImg} alt="imagem de perfil"/>
+      <div className="texto">
         <span>
-          <strong>catanacomics</strong>
-          <ion-icon name="pencil"></ion-icon>
+          <strong>{name}</strong>
+          <ion-icon onClick={changeName} name="pencil"></ion-icon>
         </span>
       </div>
     </div>
   )
 }
+
+export default User
