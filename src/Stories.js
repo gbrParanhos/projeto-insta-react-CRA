df569e694e4f192fce8baf6aa0f2397f24a4b1b3
gbrParanhos <gbrparanhos@gmail.com>
2024-07-02 00:51:24 -0300

    Stories dinâmico

--- src/Stories.js
+++ src/Stories.js
@@ -1,9 +1,21 @@
 import Story from "./Story";
 
 const Stories = () => {
+  const storyList = [
+    { alt: '9gag', src: 'assets/img/9gag.svg' },
+    { alt: 'meowed', src: 'assets/img/meowed.svg' },
+    { alt: 'barked', src: 'assets/img/barked.svg' },
+    { alt: 'nathanwpylestrangeplanet', src: 'assets/img/nathanwpylestrangeplanet.svg' },
+    { alt: 'wawawicomics', src: 'assets/img/wawawicomics.svg' },
+    { alt: 'respondeai', src: 'assets/img/respondeai.svg' },
+    { alt: 'filomoderna', src: 'assets/img/filomoderna.svg' },
+    { alt: 'memeriagourmet', src: 'assets/img/memeriagourmet.svg' }
+
+  ]
+
   return(
     <div className="stories">
-      <Story />
+      {storyList.map((storyItem,indexItem) => <Story storyItem = {storyItem} key={indexItem} />)}
       <div className="setinha">
         <ion-icon name="chevron-forward-circle"></ion-icon>
       </div>
